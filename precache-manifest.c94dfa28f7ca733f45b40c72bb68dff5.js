ce20f3ce19c0e9d91e280f2c5209f9c52c43dde1
unknown <aumi.asif@gmail.com>
2018-12-13 19:11:13 +0600

    Updates

---
+++ precache-manifest.c94dfa28f7ca733f45b40c72bb68dff5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e2965ec95230a1f4a5c039a2402fb17",
+    "url": "/aquaman_react_practice_app/static/media/slide_two.6e2965ec.jpg"
+  },
+  {
+    "revision": "4fe2ec47dcd3c7b764e6b5b0cf13bdf8",
+    "url": "/aquaman_react_practice_app/static/media/slide_three.4fe2ec47.jpg"
+  },
+  {
+    "revision": "a73dd6b05a9c1d2ee97ea644238bf2f6",
+    "url": "/aquaman_react_practice_app/static/media/slide_one.a73dd6b0.jpg"
+  },
+  {
+    "revision": "457aff962207e9c19fe2",
+    "url": "/aquaman_react_practice_app/static/js/runtime~main.457aff96.js"
+  },
+  {
+    "revision": "3136b9885d2ececaf87d",
+    "url": "/aquaman_react_practice_app/static/js/main.3136b988.chunk.js"
+  },
+  {
+    "revision": "392b5322b65dae887e9f",
+    "url": "/aquaman_react_practice_app/static/js/1.392b5322.chunk.js"
+  },
+  {
+    "revision": "3136b9885d2ececaf87d",
+    "url": "/aquaman_react_practice_app/static/css/main.ca72bd4f.chunk.css"
+  },
+  {
+    "revision": "560a15403a1b231f14397e33d8736578",
+    "url": "/aquaman_react_practice_app/index.html"
+  }
+];
